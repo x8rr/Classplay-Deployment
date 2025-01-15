edc9868df1e30c1f118a7b2380f6df0b7f214278
cobblesteve-dev <channelediting521@gmail.com>
2025-01-14 18:33:55 -0600

    Add popup thingy

---
+++ assets/js/mc-download-popup.js
@@ -0,0 +1,35 @@
+function showPopup() {
+    const popup = document.getElementById('mc-download');
+    const overlay = document.getElementById('overlay');
+    if (popup && overlay) {
+        overlay.classList.add('visible');
+        popup.style.display = 'block';
+        requestAnimationFrame(() => {
+            popup.classList.add('scale-in');
+        });
+    }
+}
+
+function hidePopup() {
+    const popup = document.getElementById('mc-download');
+    const overlay = document.getElementById('overlay');
+    if (popup && overlay) {
+        overlay.classList.remove('visible');
+        popup.classList.remove('scale-in');
+        popup.classList.add('scale-out');
+        popup.addEventListener(
+            'animationend',
+            function onAnimationEnd() {
+                popup.style.display = 'none';
+                popup.classList.remove('scale-out');
+                popup.removeEventListener('animationend', onAnimationEnd);
+            }
+        );
+    }
+}
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        hidePopup();
+    }
+});
