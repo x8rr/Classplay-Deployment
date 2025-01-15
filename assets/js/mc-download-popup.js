1b355d7b416793d05f02ce19d169e5ccd4821016
cobblesteve-dev <channelediting521@gmail.com>
2025-01-15 16:05:43 -0600

    Add Cookie Clicker Download Button

--- assets/js/mc-download-popup.js
+++ assets/js/mc-download-popup.js
@@ -28,8 +28,34 @@ function hidePopup() {
     }
 }
 
-document.addEventListener('keydown', (event) => {
-    if (event.key === 'Escape') {
-        hidePopup();
+// COOKIE CLICKER POPUP
+
+function showPopup1() {
+    const popup = document.getElementById('cc-download');
+    const overlay = document.getElementById('overlay');
+    if (popup && overlay) {
+        overlay.classList.add('visible');
+        popup.style.display = 'block';
+        requestAnimationFrame(() => {
+            popup.classList.add('scale-in');
+        });
     }
-});
+}
+
+function hidePopup1() {
+    const popup = document.getElementById('cc-download');
+    const overlay = document.getElementById('overlay');
+    if (popup && overlay) {
+        overlay.classList.remove('visible');
+        popup.classList.remove('scale-in');
+        popup.classList.add('scale-out');
+        popup.addEventListener(
+            'animationend',
+            function onAnimationEnd() {
+                popup.style.display = 'none';
+                popup.classList.remove('scale-out');
+                popup.removeEventListener('animationend', onAnimationEnd);
+            }
+        );
+    }
+}
