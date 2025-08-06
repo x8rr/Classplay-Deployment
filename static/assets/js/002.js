f6b21aa609ef613831c07f642ff16a7dbfc7700f
andrew <channelediting521@gmail.com>
2025-08-06 10:25:27 -0500

    Fix paths?

--- static/assets/js/002.js
+++ static/assets/js/002.js
@@ -247,11 +247,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
 let path = "/assets/json/a.min.json";
 if (g) {
-  path = "/assets/json/g.min.json";
+  path = "/assets/json/a.min.json";
 } else if (c) {
   path = "/assets/json/t.min.json";
 } else if (a) {
-  path = "/assets/json/a.min.json";
+  path = "/assets/json/g.min.json";
 }
 fetch(path)
   .then(response => {
